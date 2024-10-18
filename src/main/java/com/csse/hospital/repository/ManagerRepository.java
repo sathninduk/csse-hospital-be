ebe4b169935b3e928a4c88b3b766e98dc6f236a2
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-10-18 07:38:50 +0530

    chore: repository annotation

--- src/main/java/com/csse/hospital/repository/ManagerRepository.java
+++ src/main/java/com/csse/hospital/repository/ManagerRepository.java
@@ -3,6 +3,8 @@ package com.csse.hospital.repository;
 
 import com.csse.hospital.model.user.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
 }
--- src/main/java/com/csse/hospital/repository/UserRepository.java
+++ src/main/java/com/csse/hospital/repository/UserRepository.java
@@ -3,7 +3,8 @@ package com.csse.hospital.repository;
 
 import com.csse.hospital.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 }
